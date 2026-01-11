27b7b38dbc193e6aae6367f468691be1127dbcf0
Jose <josmliraa@gmail.com>
2026-01-10 23:12:13 -0600

    feat: Create HomeFragment and layout_search

---
+++ app/src/main/java/com/joselira/food_delivery_app/HomeFragment.kt
@@ -0,0 +1,20 @@
+package com.joselira.food_delivery_app
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentContainerView
+import java.time.Instant
+
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
--- app/src/main/java/com/joselira/food_delivery_app/TestComposeScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.joselira.food_delivery_app
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun TestComposeScreen() {
-    Text(text = "Compose funcionando")
-}
