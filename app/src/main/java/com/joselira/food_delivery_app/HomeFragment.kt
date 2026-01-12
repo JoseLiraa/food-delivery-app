4a90a98aa5fd6c73e3d0debc0429ae0941507b92
Jose <josmliraa@gmail.com>
2026-01-12 17:59:36 -0600

    feat: update default colors

--- app/src/main/java/com/joselira/food_delivery_app/HomeFragment.kt
+++ app/src/main/java/com/joselira/food_delivery_app/HomeFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentContainerView
-import java.time.Instant
 
 
 class HomeFragment : Fragment() {
